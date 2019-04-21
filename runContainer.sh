030fca471acdbdbd80abad7b67a7c99af0b193c5
Yangyang FU <yangyangfu@engr2-2-69-225-dhcp.int.colorado.edu>
2019-04-21 12:00:16 -0600

    Rename the script

--- runContainer.sh
+++ runContainer.sh
@@ -8,6 +8,6 @@
 	  -v `pwd`:/mnt/shared \
 	  -i \
           -t \
-	  yangyang_anaconda3 /bin/bash -c "cd /mnt/shared && python /mnt/shared/main.py"
+	  yangyang_anaconda3 /bin/bash -c "cd /mnt/shared && python /mnt/shared/play.py"
   exec docker attach anaco3
   exit $
